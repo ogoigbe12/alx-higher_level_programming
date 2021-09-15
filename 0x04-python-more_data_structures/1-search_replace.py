2056b74a316410ce45a1dc96bf92f698914bc1ed
samuel ogoigbe <ogoigbe12@gmail.com>
2021-09-15 02:33:06 -0700

    modified to search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     copy = []
-    fir x in range(len(my_list)):
+    for x in range(len(my_list)):
         if my_list[x] == search:
             copy.append(replace)
         else:
