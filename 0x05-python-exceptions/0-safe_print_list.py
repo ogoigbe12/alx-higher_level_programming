f08f52cc674f384c9e9edc86a7bf1e9a89b794b1
samuel ogoigbe <ogoigbe12@gmail.com>
2021-09-27 01:15:16 -0700

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for u in range(0, x):
+        try:
+            print("{:d}".format(my_list[u]), end="")
+            count += 1
+        except:
+            pass
+        print()
+        return count
